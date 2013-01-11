c4bc891f81516dbb3037e25b37b4144ec3d38bdc
almoral <alexmorales30@gmail.com>
2013-01-11 15:54:09 -0500

    more tests

--- app/scripts/modules/people.js
+++ app/scripts/modules/people.js
@@ -1,10 +1,32 @@
-define(['jquery'], function($) {
+define([], function() {
+	
+	var apples = [];
+
 	var sayHello = function(){
-		return 'Hi';
+		return 'Hi there';
 	};
+
+	var sayGoodbye = function(name){
+		return 'Goodbye, ' + name;
+	};
+
+	var addApple = function(typeOfApple){
+		if(apples.push(typeOfApple))
+				return true;
+
+		return false;
+	}
+
+	var countApples = function(numberOfApples){
+		return apples.length;
+	}
+
 	//List of properties for object that's being returned.
 	return {
 		//key-value pairs
-		sayHello: sayHello 
+		sayHello: sayHello,
+		sayGoodbye: sayGoodbye,
+		addApple: addApple,
+		countApples: countApples
   };
 });
